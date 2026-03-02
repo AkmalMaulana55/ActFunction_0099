d1f22ef5ed8fe388f22a57c3237cf2cffb5a98eb
AkmalMaulana55 <vylingsmall55@gmail.com>
2026-03-02 19:54:20 +0700

    input

--- chalengge.cpp
+++ chalengge.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int diagonal1, diagonal2;
 
+void input(){
+    cout << "Masukkan Diagonal 1 : ";
+    cin >> diagonal1;
+
+    cout << "Masukkan Diagonal 2 : ";
+    cin >> diagonal2;
+}
+
